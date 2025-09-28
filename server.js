9659149bbdc940e9f607a3165a40680ed313ae29
Yaathavi <yaathavi@gmail.com>
2025-09-28 03:30:24 -0400

    Fix server.js file for ECS

--- server.js
+++ server.js
@@ -102,6 +102,6 @@ app.get("/health", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
